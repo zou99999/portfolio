1a8e087a04b268e5a6a6340059c5226ffe4e7c62
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-18 19:52:15 -0700

    correction

--- global.js
+++ global.js
@@ -3,3 +3,11 @@ console.log("IT’S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+
+const currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add("current");
