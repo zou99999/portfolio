69c8e7c0d9ab27799cd6cc409d45cd6497fc6fba
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-18 20:08:42 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,12 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+//const navLinks = $$("nav a");
 
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+//const currentLink = navLinks.find(
+  //(a) => a.host === location.host && a.pathname === location.pathname
+//);
 
-if (currentLink) {
-  currentLink.classList.add("current");
+//if (currentLink) {
+  //currentLink.classList.add("current");
+//}
+
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
