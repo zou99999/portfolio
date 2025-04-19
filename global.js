539e2db8faf8bed054561299b2b2334bc5f40e85
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-18 20:42:24 -0700

    Update global.js

--- global.js
+++ global.js
@@ -56,3 +56,17 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
