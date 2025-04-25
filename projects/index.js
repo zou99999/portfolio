659b3a8e6f0c8503a3232feb4b2645b0da74df7c
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-25 15:47:04 -0700

    Update index.js

--- projects/index.js
+++ projects/index.js
@@ -1,9 +1,8 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from '/portfolio/global.js';
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
-const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
-renderProjects(latestProjects, projectsContainer, 'h2');
+renderProjects(projects, projectsContainer, 'h2');
 
 
 const githubData = await fetchGitHubData('zou99999'); // ← replace with your GitHub username
