0b07fbd52beab83e269ef907c68c03b9340a7993
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-25 14:03:35 -0700

    Update global.js

--- global.js
+++ global.js
@@ -108,9 +108,9 @@ export async function fetchJSON(url) {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
-      const data = await response.json();
-      return data;
     }
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
