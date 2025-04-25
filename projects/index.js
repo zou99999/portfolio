269933b548749640c0e7f4df2e95c4404d8a07c5
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-25 13:51:03 -0700

    Update index.js

--- projects/index.js
+++ projects/index.js
@@ -4,4 +4,3 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
-
