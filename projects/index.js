a8aa675779529c90c98d6db39058bbab87b17474
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-04-25 13:42:28 -0700

    2

---
+++ projects/index.js
@@ -0,0 +1,7 @@
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
+
