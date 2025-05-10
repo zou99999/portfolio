0b0bb6442f6cc6fd05ec11741c94050526f8dfce
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-05-09 22:12:09 -0700

    load data per commit

--- meta/main.js
+++ meta/main.js
@@ -1,13 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
-    const data = await d3.csv('loc.csv');
-    return data;
-  }
-  
-  let data = await loadData();
-
-async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
     ...row,
     line: Number(row.line), // or just +row.line
@@ -19,3 +12,37 @@ async function loadData() {
 
   return data;
 }
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,   
+        writable: false,    
+        configurable: true    
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+      });
+
+      return ret;
+    });
+}
+  
+  let data = await loadData();
+  let commits = processCommits(data);
