cb63da87d0e671e459715b6f5a9865d9f9322ff3
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-05-09 22:01:52 -0700

    Update main.js
    parse to number

--- meta/main.js
+++ meta/main.js
@@ -2,8 +2,20 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
     const data = await d3.csv('loc.csv');
-    console.log(data);
     return data;
   }
   
   let data = await loadData();
+
+async function loadData() {
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
+  return data;
+}
