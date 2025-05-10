6b07b4954b2d12e0b516e4d6bd5dffffee89ee6f
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-05-09 22:27:48 -0700

    naive scatter plot

--- meta/main.js
+++ meta/main.js
@@ -93,7 +93,42 @@ function renderCommitInfo(data, commits) {
   }
   
 
+  function renderScatterPlot(data, commits) {
+    // Put all the JS code of Steps inside this function
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+    const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+     .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+    .selectAll('circle')
+    . data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+  }
+  
   let data = await loadData();
   let commits = processCommits(data);
+  
+  renderScatterPlot(data, commits);
   renderCommitInfo(data, commits);
   console.log(commits);
+
+  
